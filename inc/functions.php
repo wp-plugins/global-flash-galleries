d8f706f12b835ae0e159cbada13bae49e38180f1
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-02 12:19:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/tags/0.4.1@201008 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/functions.php
@@ -0,0 +1,43 @@
+<?php 
+
+if (!function_exists('flgallery_versionValue')) :
+function flgallery_versionValue($version)
+{
+	$value = 0;
+	$ver = explode('.', $version);
+	$l = count($ver) - 1;
+	for ($i = $l; $i >= 0; $i--)
+		$value += $ver[$i] * pow(100, 3-$i);
+
+	return $value;
+}
+endif;
+
+if (!function_exists('file_put_contents')) :
+function file_put_contents($filename, $data)
+{
+	$fp = fopen($filename, 'rb');
+	$res = fwrite($fp, $data);
+	fclose($fp);
+
+	return $res;
+}
+endif;
+
+if (!function_exists('strisplashes')) :
+function strisplashes($text) {
+	return $text;
+}
+endif;
+
+
+if (!function_exists('print_pre')) {
+function print_pre($var)
+{
+	print '<pre>';
+	print_r($var);
+	print '</pre>';
+}}
+
+
+?>
