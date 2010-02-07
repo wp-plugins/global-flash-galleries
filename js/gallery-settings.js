db54b43e5b5dddfb72162020a613bb4aa1de5622
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-07 17:33:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/trunk@202899 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/gallery-settings.js
@@ -0,0 +1,215 @@
+document.getElementById('gallery-options').reset();
+
+function flashAPI(swf, callback, timeout)
+{
+	if (swf.parentNode.parentNode.style.display != 'none')
+	{
+		if (timeout == undefined || --timeout)
+		{
+			if (swf.setParameterValue == undefined)
+			{
+				setTimeout( function() { flashAPI(swf, callback, timeout); }, 1000 );
+			}
+			else
+			{
+				//try {
+					callback(swf);
+				//}
+				//catch (e) {
+				//	alert(e);
+				//}
+			}
+		}
+	}
+}
+
+function getValue(e)
+{
+	if (e.type == 'checkbox')
+	{
+		return e.value = e.checked ? 'true' : 'false';
+	}
+	else if (e.className.match(/\bcolor\b/))
+	{
+		return '0x'+e.value.match(/[0-9A-Fa-f]+/);
+	}
+	else
+		return e.value;
+}
+
+function getValuesByTagName(tagName, e)
+{
+	var result = new Array();
+	if (e)
+	{
+		var elements = e.getElementsByTagName(tagName);
+		var m;
+		for (var i = 0; i < elements.length; i++)
+		{
+			if (m = elements[i].id.match(/(.+)\.(.+)/))
+				result.push(m[2] + ': "' + getValue(elements[i]) + '"');
+		}
+	}
+	return result;
+}
+
+function applyChanges(e)
+{
+	var m;
+	if ( m = e.id.match(/(.+)\.(.+)/) )
+	{
+		var obj = document.getElementById(m[1]);
+
+		var elements = new Array();
+
+		elements = elements.concat(getValuesByTagName('input', obj));
+		elements = elements.concat(getValuesByTagName('select', obj));
+		elements = elements.concat(getValuesByTagName('textarea', obj));
+
+		var objValues = '';
+		for (var i = 0; i < elements.length; i++)
+		{
+			objValues += elements[i] + ', ';
+		}
+		objValues = objValues.substring(0, objValues.length-2);
+
+		flashAPI(flgallery.swf,
+			function($)
+			{
+				document.getElementById('debug').innerHTML = '$.setParameterValue( "'+ m[1] +'", { '+ objValues +' } );';
+				eval( '$.setParameterValue( "'+ m[1] +'", { '+ objValues +' } );' );
+			},
+			30
+		);
+	}
+	else
+	{
+		flashAPI(flgallery.swf,
+			function($)
+			{
+				document.getElementById('debug').innerHTML = '$.setParameterValue( "'+ e.id +'", "'+ getValue(e) +'" );';
+				$.setParameterValue( e.id, getValue(e) );
+			},
+			30
+		);
+	}
+};
+
+jQuery(document).ready(function($) {
+	$('#flashSettings .objectProperties').hide();
+
+	$('#flashSettings input, #flashSettings select, #flashSettings textarea').change(
+		function() {
+			applyChanges(this);
+		}
+	);
+
+	$('#flashSettings input.int').keypress(
+		function(e) {
+			if (e.which != 0 && e.which != 8 && String.fromCharCode(e.which).match(/\D/))
+				return false;
+		}
+	);
+
+	/*
+	// defaults
+	$.colorInput.defaults.cells = 32;
+	$.colorInput.defaults.cellSize = 3;
+	$.colorInput.defaults.hueWidth = 20;
+	$.colorInput.defaults.hideInput = false;
+	$.colorInput.defaults.hoverSelect = false;
+	$.colorInput.defaults.cancelOnClick = [];
+	$.colorInput.defaults.acceptCancelButtons = true;
+	$.colorInput.defaults.showLeft = false;
+	$.colorInput.defaults.noHash = true;
+
+	// set up the color input
+	var $input = $("#flashSettings input.color").colorInput({
+		colorSelected: function(e) {
+			this.value = this.value.match(/[0-9A-F-a-f]+/);
+		}
+	});
+
+	// check with user if they want to keep the selection
+	$(document).click(function(e) {
+		if ($.colorInput.current) {
+			if ($.colorInput.current.isChanged && window.confirm("Accept selected colour change?")) {
+				$.colorInput.current.accept();
+			} else {
+				$.colorInput.current.cancel();
+			}
+		}
+	});
+	*/
+	
+	//$('#colorPicker').farbtastic('input.color');
+	
+	/*$('input.color').click(
+		function()
+		{
+			$('#colorPicker').farbtastic(this);
+		}
+	);*/
+
+	$('#flashSettings .objectName label').toggle(
+		function() {
+			//$('#flashSettings .objectName .opened').click();
+			$(this).addClass("opened");
+			$('#'+this.htmlFor).slideDown(500);
+		},
+		function() {
+			$(this).removeClass("opened");
+			$('#'+this.htmlFor).slideUp(500);
+		}
+	);
+
+
+	$('#flash-preview').draggable({ handle: '.hndle', cursor: 'move' /*, containment: '.wrap'*/ });
+	
+	var
+		resizeDeltaX = 0,
+		resizeDeltaY = 0;
+
+	function flashPreviewResizable(selector)
+	{
+		$('#flash-preview').resizable({
+			autoHide: true,
+			alsoResize: '#flash-preview object, #flash-preview .inside2',
+			handles: 'e, se, s, sw, w',
+			start: function(event, ui) {
+				resizeDeltaX = ui.size.width - $('#gallery\\.width').attr('value');
+				resizeDeltaY = ui.size.height - $('#gallery\\.height').attr('value');
+
+				$('#gallery-options .gallery, #gallery-options .settings, #gallery-options .submit').css('z-index', 0);
+				$(this).resizable('option', 'minWidth', 300 + resizeDeltaX);
+				$(this).resizable('option', 'minHeight', 200 + resizeDeltaY);
+				
+				$('#flash-preview .inside3').css('display', 'none');
+				$('#flash-preview .inside2').css('background-image', 'url('+ flgallery.pluginURL +'/img/transparent.gif)');
+			},
+			resize: function(event, ui) {
+				$('#gallery\\.width').attr('value', ui.size.width - resizeDeltaX);
+				$('#gallery\\.height').attr('value', ui.size.height - resizeDeltaY);
+			},
+			stop: function() {
+				$('#flash-preview .inside3').css('display', 'block');
+				$('#flash-preview .inside2').css('background-image', 'url('+ flgallery.pluginURL +'/img/transparent-50.gif)');
+				$('#gallery-options .gallery, #gallery-options .settings, #gallery-options .submit').css('z-index', 20);
+			}
+		});
+	}
+	flashPreviewResizable();
+
+	$('#flash-preview .handlediv').toggle(
+		function() {
+			$('#flash-preview').resizable('destroy');
+			$('#flash-preview').height('auto');
+			$('#flash-preview .inside').slideUp(250);
+		},
+		function() {
+			$('#flash-preview .inside').slideDown(500);
+			flashPreviewResizable();
+		}
+	);
+	
+});
