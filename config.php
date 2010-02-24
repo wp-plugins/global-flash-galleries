5849f63ffe9e63f32b31bacb019b750ce706d77d
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-24 15:38:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/tags/0.4.6@209840 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,44 @@
+<?php 
+
+define( 'FLGALLERY_NAME',		'flgallery' );
+
+
+define( 'FLGALLERY_DEBUG',		false	);		
+define( 'FLGALLERY_WARNINGS',	true	);		
+define( 'FLGALLERY_ERRORS',		true	);		
+
+
+define( 'FLGALLERY_DB_PREFIX',		FLGALLERY_NAME.'_'	);	
+define( 'FLGALLERY_DB_GALLERIES',	'galleries'	);			
+define( 'FLGALLERY_DB_SETTINGS',	'settings'	);			
+define( 'FLGALLERY_DB_IMAGES',		'images'	);			
+define( 'FLGALLERY_DB_ALBUMS',		'albums'	);			
+
+
+define( 'FLGALLERY_CONTENT',	FLGALLERY_NAME	);	
+define( 'FLGALLERY_IMAGES',		'images'	);		
+define( 'FLGALLERY_XML',		'xml'		);		
+define( 'FLGALLERY_TPL',		'tpl'		);		
+define( 'FLGALLERY_UPLOADS',	'uploads'	);		
+define( 'FLGALLERY_TEMP',		'tmp'		);		
+
+
+define( 'FLGALLERY_PLUGIN_DIR', dirname(__FILE__) );
+define( 'FLGALLERY_PLUGIN_URL', plugins_url('', __FILE__) );
+
+define( 'FLGALLERY_INCLUDE', FLGALLERY_PLUGIN_DIR.'/inc' );
+
+define( 'FLGALLERY_GLOBALS', FLGALLERY_INCLUDE.'/globals.php' );
+
+define( 'FLGALLERY_TPL_DIR', FLGALLERY_PLUGIN_DIR.'/tpl' );
+
+define( 'FLGALLERY_CONTENT_DIR', WP_CONTENT_DIR.'/'.FLGALLERY_CONTENT );
+define( 'FLGALLERY_CONTENT_URL', WP_CONTENT_URL.'/'.FLGALLERY_CONTENT );
+
+define( 'FLGALLERY_LOG', FLGALLERY_CONTENT_DIR.'/log.txt' );
+
+
+define( 'flgallery_defaultImageQuality', 85 );
+
+
+?>
---
+++ popup.php
@@ -0,0 +1,31 @@
+<?php 
+error_reporting(0);
+
+$gallery_id = (int)$_REQUEST['id'];
+if ( empty($gallery_id) )
+	exit();
+
+$wp_config = '../../../wp-config.php';
+if ( !file_exists($wp_config) ) $wp_config = $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
+require_once $wp_config;
+
+$gallery = new flgalleryGallery($gallery_id);
+$galleryHTML = $flgalleryPlugin->flashGallery( array('id' => $gallery->id) );
+
+;echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>'; echo $gallery->name; ;echo '</title>
+<style type="text/css">
+body {
+	margin: 0;
+}
+</style>
+</head>
+
+<body>
+'; echo $galleryHTML; ;echo '
+</body>
+</html>';
+?>
