454a1a9fda9ad1d7cb9e02de109d7294fcac34d9
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-27 16:13:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/tags/0.4.7@211022 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gallery-xml.php
@@ -0,0 +1,41 @@
+<?php 
+ob_start();
+error_reporting(0);
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: '.date('r'));
+
+if ( !empty($_REQUEST['id']) )
+{
+	require_once 'config.php';
+
+	$gallery_id = (int)$_REQUEST['id'];
+
+	preg_match('|(.*?/)(wp-content)/.*|i', str_replace('\\', '/', __FILE__), $m);
+	$abspath = $m[1];
+	$content_dir = $m[1].$m[2];
+
+	$flgalleryXmlFile = $content_dir.'/'.FLGALLERY_CONTENT.'/'.FLGALLERY_XML."/{$gallery_id}.xml";
+	if ( file_exists($flgalleryXmlFile) )
+	{
+		header('Content-Type: text/xml; charset=utf-8');
+		ob_end_clean();
+
+		readfile($flgalleryXmlFile);
+	}
+	else
+	{
+		require_once $abspath.'wp-config.php';
+
+		$flgalleryGallery = new flgalleryGallery( $gallery_id );
+
+		header('Content-Type: text/xml; charset=utf-8');
+		ob_end_clean();
+
+		$flgalleryGallery->xml(true);
+	}
+}
+exit();
+
+
+?>
