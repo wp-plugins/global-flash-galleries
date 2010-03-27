9a77620ada3060b109a23f2a320fd30fdd7a5e36
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-27 14:10:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/tags/0.5.5@222044 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/db.galleries.php
@@ -0,0 +1,22 @@
+<?php 
+if (!defined('WP_ADMIN')) { header('HTTP/1.0 403 Forbidden'); exit('Access denied'); }
+
+require_once ABSPATH.'wp-admin/includes/upgrade.php';
+dbDelta("
+	CREATE TABLE `{$table_name}` (
+		`id`			BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+		`order`			BIGINT NOT NULL DEFAULT '0',
+		`author`		BIGINT UNSIGNED NOT NULL,
+		`type`			VARCHAR(20) NOT NULL,
+		`name`			VARCHAR(255) NOT NULL,
+		`width`			INT UNSIGNED NOT NULL DEFAULT '0',
+		`height`		INT UNSIGNED NOT NULL DEFAULT '0',
+		`created`		DATETIME NOT NULL,
+		`modified`		DATETIME NOT NULL,
+		PRIMARY KEY		(`id`),
+		KEY				(`author`, `order`, `created`, `modified`)
+	) DEFAULT CHARSET = {$wpdb->charset}
+");
+
+
+?>
