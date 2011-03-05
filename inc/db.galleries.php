13507dc3cd852d4fbea30eb7a57c7fd76e45b308
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-05 19:35:30 +0000

    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/tags/0.7.12@355844 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/db.galleries.php
@@ -0,0 +1,21 @@
+<?php 
+if (!defined('FLGALLERY_VERSION')) { header('HTTP/1.0 403 Forbidden'); exit('Access denied'); }
+
+$query = "
+	CREATE TABLE `{$table_name}` (
+		`id`			BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+		`order`			BIGINT NOT NULL DEFAULT '0',
+		`author`		BIGINT UNSIGNED NOT NULL,
+		`type`			VARCHAR(20) NOT NULL,
+		`name`			VARCHAR(255) NOT NULL,
+		`width`			INT UNSIGNED NOT NULL DEFAULT '0',
+		`height`		INT UNSIGNED NOT NULL DEFAULT '0',
+		`created`		DATETIME NOT NULL,
+		`modified`		DATETIME NOT NULL,
+		PRIMARY KEY		(`id`),
+		KEY				(`author`, `order`, `created`, `modified`)
+	){$charset_collate}
+";
+
+
+?>
