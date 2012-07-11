3156e0737bb3d77d20e3690046d3e0b08cd61522
flipper <flipper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-11 13:13:14 +0000

    new 0.12.0 version
    git-svn-id: https://plugins.svn.wordpress.org/global-flash-galleries/tags/global-flash-galleries-0.12.0@570613 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/site.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class flgallerySite extends flgalleryBaseClass
+{
+	var
+		$name,
+		$title,
+		$url,
+		$dir;
+
+	function init()
+	{
+		$this->url = rtrim(get_option('siteurl'), '/');
+	}
+}
+
+?>
